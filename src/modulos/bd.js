eac5307650576f5dd495d27a8cdd2b0b7564eb68
Daniel Salazar <juan.salazar111111@soy.sena.edu.co>
2024-12-09 18:39:41 -0500

    Actualizacion de proyecto para despliegue

--- src/modulos/bd.js
+++ src/modulos/bd.js
@@ -7,17 +7,18 @@ const mysql = require("mysql2"); // PRINCIPIO DE INMUTABILIDAD
 // CADENA DE CONEXION
 // Create the connection to database
 const cnx = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  database: "interpolice"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 // A simple SELECT query
 
- cnx.query("SELECT * FROM citizen",(err, results) => {
+/*  cnx.query("SELECT * FROM citizen",(err, results) => {
     console.log(results); // results contains rows returned by server
     
- })
+ }) */
 
 
 cnx.connect((error) => {
